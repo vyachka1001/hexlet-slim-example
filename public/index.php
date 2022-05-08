102d96aa32197ed462816701db00be7f0f88e9c1
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-05-08 12:58:01 +0300

    add post route to users and router

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
 use DI\Container;
+use App\Validator;
 
 $container = new Container();
 $container->set('renderer', function () {
@@ -14,9 +15,11 @@ $app->addErrorMiddleware(true, true, true);
 
 $users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
 
+$router = $app->getRouteCollector()->getRouteParser();
+
 $app->get('/', function ($request, $response) {
     return $response->write('Welcome to Slim!');
-});
+})->setName('root');
 
 $app->get('/users', function ($request, $response, array $args) use ($users) {
     $term = $request->getQueryParam('term', '');
@@ -25,20 +28,40 @@ $app->get('/users', function ($request, $response, array $args) use ($users) {
         'term' => $term
     ];
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
-});
+})->setName('users');
 
-$app->post('/users', function ($request, $response) {
-    return $response->withStatus(302);
-});
+$app->post('/users', function ($request, $response) use ($router) {
+    $validator = new Validator();
+    $user = $request->getParsedBodyParam('user');
+    $errors = $validator->validate($user);
+    if (count($errors) === 0) {
+        $encodedData = json_encode($user);
+        file_put_contents('./public/user.json', $encodedData);
+        return $response->withRedirect($router->urlFor('users'), 302);
+    }
+    $params = [
+        'user' => $user,
+        'errors' => $errors
+    ];  
+    return $this->get('renderer')->render($response, "users/new.phtml", $params);
+})->setName('addUser');
 
-$app->get('/users/{id}', function ($request, $response, $args) {
+$app->get('/users/show/{id}', function ($request, $response, $args) {
     $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
-});
+})->setName('user');
+
+$app->get('/users/new', function ($request, $response, $args) {
+    $params = [
+        'user' => ['name' => '', 'email' => ''],
+        'errors' => []
+    ];
+    return $this->get('renderer')->render($response, 'users/new.phtml', $params);
+})->setName('newUser');
 
 $app->get('/courses/{id}', function ($request, $response, array $args) {
     $id = $args['id'];
     return $response->write("Course id: {$id}");
-});
+})->setName('course');
 
 $app->run();
